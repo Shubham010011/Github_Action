c5c3a4958b686b8289873f9101fef51c9e63c590
Shubham Sharma <yashsharma6799@gmail.com>
2026-01-21 09:55:03 +0530

    Add files via upload

---
+++ src/test/java/com/tyit/CalculaterTest.java
@@ -0,0 +1,14 @@
+package com.tyit;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculaterTest {
+
+    @Test
+    public void testAddition() {
+        Calculater c = new Calculater();
+        assertEquals(10, c.add(5, 5));
+    }
+}
+
